fe88af9295d1ac1eb00336b15bdc13dcfa2c415f
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2018-03-22 13:33:14 -0300

    Changes in API and little ideas about arguments.

--- lib/schedule_notifications.dart
+++ lib/schedule_notifications.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class ScheduleNotifications {
@@ -7,8 +6,11 @@ class ScheduleNotifications {
   static const MethodChannel _channel =  const MethodChannel('schedule_notifications');
 
   // TODO: Must add doc
-  static Future<Null> schedule(text, time, [repeatIn]) {
-    return _channel.invokeMethod('scheduleNotification', [text, time, [repeatIn]]);
+  // text: text to be showed
+  // time: hour in minutes or use DateTime
+  // repeatAt: [1..7], when 1 is Sunday. check if its ok
+  static Future<Null> schedule(text, time, [repeatAt]) {
+    return _channel.invokeMethod('scheduleNotification', [text, time, [repeatAt]]);
   }
 
   static Future<Null> unschedule() {
